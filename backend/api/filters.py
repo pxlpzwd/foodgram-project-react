e1e327d1cd1c0c8851cb301b51b8fc5c87fb7cf1
pxlpzwd <pxlpzwd@anon-44.ru>
2023-09-25 14:13:16 +0300

    fix pep8

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,5 @@
-from django_filters import rest_framework as filters
-
 from core.services import maybe_incorrect_layout
+from django_filters import rest_framework as filters
 from recipes.models import Ingredient, Recipe
 
 
