27dfbf1ac98f3a9e87fa3f9f8279a505c3347f7d
pxlpzwd <pxlpzwd@anon-44.ru>
2023-09-25 14:22:42 +0300

    test

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,14 +1,13 @@
 from collections import OrderedDict
 
+from core.services import recipe_ingredients_set
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.db.models import F, QuerySet
 from django.db.transaction import atomic
 from drf_extra_fields.fields import Base64ImageField
-from rest_framework.serializers import ModelSerializer, SerializerMethodField
-
-from core.services import recipe_ingredients_set
 from recipes.models import Ingredient, Recipe, Tag
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 User = get_user_model()
 
