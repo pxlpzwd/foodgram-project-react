f73473d1fa9065cc953052b4d761f21d2c217030
pxlpzwd <pxlpzwd@anon-44.ru>
2023-09-29 22:03:31 +0300

    test8

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -111,6 +111,8 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ],
+    'DEFAULT_PAGINATION_CLASS': 'api.apps.core.pagination.PageLimitPagination',
+    'PAGE_SIZE': 10
 }
 
 DJOSER = {
